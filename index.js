4b568096df7edbbcebfe3c1a9766e0d666bc025c
Elizabeth Njuguna <elizabethnjuguna217@gmail.com>
2025-03-10 07:15:08 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 
 // Write your code here
+// Define the variables
+let num1 = 31; // Choose a value for num1
+let num2 = 2;  // Choose a value for num2
+
+// Multiply num1 and num2 and assign the result to the variable multiply
+let multiply = num1 * num2;
+
+// Verify the result
+console.log(multiply); // Output: 62
+
+// random variable
+let random = Math.floor((Math.random()*10)) + 1
+
+let num3 = 19;
+let num4 = 5;
+let mod = num3 % num4;
+
+console.log(mod); // Output: 4
+
+// Define a set of numbers
+const numbers = [10, 5, 20, 15, 8];
+
+// Find the highest number in the set
+let max = Math.max(...numbers);
+
+// Verify the result
+console.log(max); // Output: 20
